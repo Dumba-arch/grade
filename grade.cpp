4bd9f4d48e88fbef58438218f9cc0337fbc74c22
Dumba-arch <victorious651@gmail.com>
2024-09-22 11:57:04 +0300

    Add files via upload

---
+++ grade.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+int main() {
+    int grade;
+
+    do {
+        cout << "Please enter your grade (0-100): ";
+        cin >> grade;
+
+        if (grade < 0 || grade > 100) {
+            cout << "Invalid input. Please enter a grade between 0 and 100." <<endl;
+        }
+    } while (grade < 0 || grade > 100);
+
+    cout << "Thank you! Your grade is: " << grade << endl;
+    return 0;
+}
+
